1bcffdca0b13b690248814bb306b11640c028b7a
Nat <nat_to_kung@hotmail.com>
2026-04-04 16:23:09 +0700

    refactor; style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage/LandingPage'
 import CalculatorPage from './components/CalculatorPage/CalculatorPage'
-import './App.css'
 
 export default function App() {
   return (
